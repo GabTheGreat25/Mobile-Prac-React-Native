42b1ec75787c129b50f438e10d37bb0ee5d07c58
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-05 00:18:06 +0800

    Simple Custom Component

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
-import { View, Alert, Button } from "react-native";
+import { View } from "react-native";
+import { Greet } from "./components";
 
 export default function App() {
   return (
     <>
       <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-        <Button title="Alert" onPress={() => Alert.alert("Invalid data!")} />
-        <Button
-          title="Alert 2"
-          onPress={() => Alert.alert("invalid data!", "DOB incorrect!")}
-        />
-        <Button
-          title="Alert 3"
-          onPress={() =>
-            Alert.alert("invalid data!", "DOB incorrect!", [
-              {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-              },
-              {
-                text: "OK",
-                onPress: () => console.log("OK Pressed"),
-              },
-            ])
-          }
-        />
+        <Greet name="Gabriel Mendoza" />
       </View>
     </>
   );
---
+++ components/Greet/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ({ name }) {
+  return (
+    <View>
+      <Text>Hello, {name}!</Text>
+    </View>
+  );
+}
