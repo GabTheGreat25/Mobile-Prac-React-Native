cd8f5b28949b85546dca8eabbd1656179d92aff5
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-05 00:50:46 +0800

    Simple StyleSheet

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Greet } from "./components";
 
 export default function App() {
   return (
     <>
-      <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <View style={styles.container}>
         <Greet name="Gabriel Mendoza" />
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+});
