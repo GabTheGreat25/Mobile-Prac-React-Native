209d4eeda0d99706b7fbaf97e7f5bc7460c67ce8
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-04 12:13:56 +0800

    Simple ScrollView

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
-import { Image, ImageBackground, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <>
       <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-        <Text>Hello World</Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <View>
-          <ImageBackground source={logoImg}>
-            <Text>Inside</Text>
-          </ImageBackground>
-        </View>
+        <ScrollView>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+          <Text>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            auctor, nunc eget ultricies ultricies, nunc nisl ultricies tortor,
+            quis lacinia nunc nunc eget nunc. Donec auctor, nunc eget ultricies
+            ultricies, nunc nisl ultricies tortor, quis lacinia nunc nunc eget
+            nunc. Donec auctor, nunc eget ultricies ultricies, nunc nisl
+            ultricies tortor, quis lacinia nunc nunc eget nunc. Donec auctor,
+            nunc eget ultricies ultricies, nunc nisl ultricies tortor, quis
+            lacinia nunc nunc eget nunc. Donec auctor, nunc eget ultricies
+            ultricies, nunc nisl ultricies tortor, quis lacinia nunc nunc eget
+            nunc. Donec auctor, nunc eget ultricies ultricies, nunc nisl
+            ultricies tortor, quis lacinia nunc nunc eget nunc. Donec auctor,
+            nunc eget ultricies ultricies, nunc nisl ultricies tortor, quis
+            lacinia nunc nunc eget nunc. Donec auctor, nunc eget ultricies
+            ultricies, nunc nisl ultricies tortor, quis lacinia nunc nunc eget
+            nunc.
+          </Text>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </ScrollView>
       </View>
     </>
   );
