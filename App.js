1f50f77aae3d101b1f8e54514c7c3e80254b40c9
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-04 12:13:36 +0800

    Simple Image and ImageBackground

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <>
       <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
         <Text>Hello World</Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <View>
+          <ImageBackground source={logoImg}>
+            <Text>Inside</Text>
+          </ImageBackground>
+        </View>
       </View>
     </>
   );
