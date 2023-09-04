a11b9d0fc33d04356c090471a40807a5e8009701
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-04 17:49:07 +0800

    Simple Pressable

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Button, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, View, Button } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
@@ -7,10 +7,34 @@ export default function App() {
       <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
         <Button
           title="Press Me"
-          onPress={() => console.log("I am pressed!")}
+          onPress={() => console.log("Button pressed!")}
           color="midnightblue"
-          disabled={true}
+          disabled={false}
         />
+        <ScrollView>
+          <Pressable onPress={() => console.log("Image pressed!")}>
+            <Image source={logoImg} style={{ width: 300, height: 300 }} />
+          </Pressable>
+          <Pressable onPress={() => console.log("Text pressed!")}>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              auctor, nunc eget ultricies ultricies, nunc nisl ultricies tortor,
+              quis lacinia nunc nunc eget nunc. Donec auctor, nunc eget
+              ultricies ultricies, nunc nisl ultricies tortor, quis lacinia nunc
+              nunc eget nunc. Donec auctor, nunc eget ultricies ultricies, nunc
+              nisl ultricies tortor, quis lacinia nunc nunc eget nunc. Donec
+              auctor, nunc eget ultricies ultricies, nunc nisl ultricies tortor,
+              quis lacinia nunc nunc eget nunc. Donec auctor, nunc eget
+              ultricies ultricies, nunc nisl ultricies tortor, quis lacinia nunc
+              nunc eget nunc. Donec auctor, nunc eget ultricies ultricies, nunc
+              nisl ultricies tortor, quis lacinia nunc nunc eget nunc. Donec
+              auctor, nunc eget ultricies ultricies, nunc nisl ultricies tortor,
+              quis lacinia nunc nunc eget nunc. Donec auctor, nunc eget
+              ultricies ultricies, nunc nisl ultricies tortor, quis lacinia nunc
+              nunc eget nunc.
+            </Text>
+          </Pressable>
+        </ScrollView>
       </View>
     </>
   );
