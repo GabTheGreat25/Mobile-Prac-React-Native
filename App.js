a6b08521add3d01345876abac240974a4bff0903
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-04 12:20:04 +0800

    Simple Button

--- App.js
+++ App.js
@@ -1,31 +1,16 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { Button, View } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <>
       <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-        <ScrollView>
-          <Image source={logoImg} style={{ width: 300, height: 300 }} />
-          <Text>
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            auctor, nunc eget ultricies ultricies, nunc nisl ultricies tortor,
-            quis lacinia nunc nunc eget nunc. Donec auctor, nunc eget ultricies
-            ultricies, nunc nisl ultricies tortor, quis lacinia nunc nunc eget
-            nunc. Donec auctor, nunc eget ultricies ultricies, nunc nisl
-            ultricies tortor, quis lacinia nunc nunc eget nunc. Donec auctor,
-            nunc eget ultricies ultricies, nunc nisl ultricies tortor, quis
-            lacinia nunc nunc eget nunc. Donec auctor, nunc eget ultricies
-            ultricies, nunc nisl ultricies tortor, quis lacinia nunc nunc eget
-            nunc. Donec auctor, nunc eget ultricies ultricies, nunc nisl
-            ultricies tortor, quis lacinia nunc nunc eget nunc. Donec auctor,
-            nunc eget ultricies ultricies, nunc nisl ultricies tortor, quis
-            lacinia nunc nunc eget nunc. Donec auctor, nunc eget ultricies
-            ultricies, nunc nisl ultricies tortor, quis lacinia nunc nunc eget
-            nunc.
-          </Text>
-          <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        </ScrollView>
+        <Button
+          title="Press Me"
+          onPress={() => console.log("I am pressed!")}
+          color="midnightblue"
+          disabled={true}
+        />
       </View>
     </>
   );
