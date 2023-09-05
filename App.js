4141cc667f628c52616a8fbedcb032dd8b139cca
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-05 23:50:06 +0800

    Simple gap

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     // flex: 1,
+    gap: 10,
     height: 300,
     flexWrap: "wrap",
     alignContent: "center",
