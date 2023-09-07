f1be60638e6bdd8da0d0fc69eab99bdfca1a6760
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-07 16:02:38 +0800

    Simple Relative and Absolute Position

--- App.js
+++ App.js
@@ -6,10 +6,21 @@ export default function App() {
     <>
       <View style={styles.container}>
         <Greet name="Gabriel Mendoza" />
-        <Box style={{ backgroundColor: "#8e9b00", flexGrow: 1 }}>Box 1</Box>
-        <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
-        <Box style={{ backgroundColor: "#1c4c56", flexGrow: 2 }}>Box 3</Box>
-        <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+        <Box
+          style={{
+            backgroundColor: "#8e9b00",
+            width: "50%",
+            left: 200,
+            bottom: 100,
+            position: "absolute",
+            zIndex: 1000,
+          }}
+        >
+          Box 1
+        </Box>
+        <Box style={{ backgroundColor: "#b65d1f", width: "50%" }}>Box 2</Box>
+        <Box style={{ backgroundColor: "#1c4c56", width: "50%" }}>Box 3</Box>
+        <Box style={{ backgroundColor: "#ab9156", width: "50%" }}>Box 4</Box>
       </View>
     </>
   );
@@ -18,10 +29,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "stretch",
+    height: 100,
     backgroundColor: "#fff",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
+    position: "relative",
   },
 });
