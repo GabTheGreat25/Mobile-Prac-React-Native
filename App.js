5849d058d85a4b2c32fcd0bf9c6bf0c46db8c0ce
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-07 17:52:47 +0800

    Simple useWindowDimensions

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Greet, Box } from "./components";
+import { View, Text, useWindowDimensions, StyleSheet } from "react-native";
 
 export default function App() {
+  const { height: windowHeight, width: windowWidth } = useWindowDimensions();
+
   return (
     <>
       <View style={styles.container}>
-        <Greet name="Gabriel Mendoza" />
-        <Box
-          style={{
-            backgroundColor: "#8e9b00",
-            width: "50%",
-            left: 200,
-            bottom: 100,
-            position: "absolute",
-            zIndex: 1000,
-          }}
+        <View
+          style={[
+            styles.box,
+            {
+              width: windowWidth > 500 ? "70%" : "90%",
+              height: windowHeight > 500 ? "60%" : "90%",
+            },
+          ]}
         >
-          Box 1
-        </Box>
-        <Box style={{ backgroundColor: "#b65d1f", width: "50%" }}>Box 2</Box>
-        <Box style={{ backgroundColor: "#1c4c56", width: "50%" }}>Box 3</Box>
-        <Box style={{ backgroundColor: "#ab9156", width: "50%" }}>Box 4</Box>
+          <Text style={{ fontSize: windowWidth > 1000 ? 50 : 24 }}>
+            Window Width: {windowWidth}
+          </Text>
+          <Text style={{ fontSize: windowHeight > 1000 ? 50 : 24 }}>
+            Window Height: {windowHeight}
+          </Text>
+        </View>
       </View>
     </>
   );
@@ -29,11 +30,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 100,
-    backgroundColor: "#fff",
-    marginTop: 64,
-    borderWidth: 6,
-    borderColor: "red",
-    position: "relative",
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
