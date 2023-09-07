02629b0e8df67f0b05b72aa51eb6fe673ec8d955
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-08 00:31:33 +0800

    Simple safeAreaView for notch

--- App.js
+++ App.js
@@ -1,33 +1,45 @@
-import { View, Text, useWindowDimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  useWindowDimensions,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   const { height: windowHeight, width: windowWidth } = useWindowDimensions();
 
   return (
     <>
-      <View style={styles.container}>
-        <View
-          style={[
-            styles.box,
-            {
-              width: windowWidth > 500 ? "70%" : "90%",
-              height: windowHeight > 500 ? "60%" : "90%",
-            },
-          ]}
-        >
-          <Text style={{ fontSize: windowWidth > 1000 ? 50 : 24 }}>
-            Window Width: {windowWidth}
-          </Text>
-          <Text style={{ fontSize: windowHeight > 1000 ? 50 : 24 }}>
-            Window Height: {windowHeight}
-          </Text>
+      <SafeAreaView style={styles.safeContainer}>
+        <View style={styles.container}>
+          <View
+            style={[
+              styles.box,
+              {
+                width: windowWidth > 500 ? "70%" : "90%",
+                height: windowHeight > 500 ? "60%" : "90%",
+              },
+            ]}
+          >
+            <Text style={{ fontSize: windowWidth > 1000 ? 50 : 24 }}>
+              Window Width: {windowWidth}
+            </Text>
+            <Text style={{ fontSize: windowHeight > 1000 ? 50 : 24 }}>
+              Window Height: {windowHeight}
+            </Text>
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
   container: {
     flex: 1,
     backgroundColor: "plum",
